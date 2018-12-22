6026a5fcb7e9233889363d530916fedae547bc26
Sagar <sgavhane70@gmail.com>
2018-12-22 18:01:01 +0530

    react hook error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
-import Login from './screens/auth/Login'
+import Login from './screens/auth/Login/Login'
 import './styles/main.css'
 
 ReactDOM.render(<Login />, document.getElementById('root'))
