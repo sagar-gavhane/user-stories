8c5bbe81592eb389822aad8f5ff9143072c9d500
Sagar <sgavhane70@gmail.com>
2019-01-15 06:19:35 +0530

    removed boilerplate code and focus on creting markdown based template

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import * as serviceWorker from './serviceWorker'
-import Login from './screens/auth/Login/Login'
-import './styles/main.css'
-
-ReactDOM.render(<Login />, document.getElementById('root'))
-
-serviceWorker.unregister()
